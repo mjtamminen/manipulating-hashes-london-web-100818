a98be220190bbe310a89879f9a6d19b7a9707cd2
tamminen.mikkoj <tamminen.mikkoj@gmail.com>
2018-10-04 03:05:14 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,12 +1,12 @@
 def second_challenge
   groceries = {
-   dairy: ["milk", "yogurt", "cheese"],
-   vegetable: ["carrots", "broccoli", "cucumbers"],
-   meat: ["chicken", "steak", "salmon"],
-   grains: ["rice", "pasta"]
+               dairy:     ["milk", "yogurt", "cheese"],
+               vegetable: ["carrots", "broccoli", "cucumbers"],
+               meat:      ["chicken", "steak", "salmon"],
+               grains:    ["rice", "pasta"]
   }
 
-  #code your solution here!
+
 
   
 
